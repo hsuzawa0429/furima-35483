bea78dfa87d1b94ca65f666812fe021905c0c40e
hsuzawa0429 <hiroyuki.suzawa0429@gmail.com>
2021-07-31 14:06:38 +0900

    モデル単体テストコードの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -57,25 +57,25 @@ RSpec.describe Item, type: :model do
     it '販売価格についての情報が必須であること' do
       @item.price = ''
       @item.valid?
-      expect(@item.errors.full_messages).to include("Price can't be blank", "Price is too short (minimum is 300 characters)", "Price is invalid")
+      expect(@item.errors.full_messages).to include("Price can't be blank", "Price is invalid", "Price is not a number")
     end
 
     it '販売価格は、¥300~¥9,999,999の間のみ保存可能であること' do
       @item.price = 100
       @item.valid?
-      expect(@item.errors.full_messages).to include("Price is too short (minimum is 300 characters)")
+      expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
     end
 
     it '販売価格は、¥300~¥9,999,999の間のみ保存可能であること' do
       @item.price = 10000000
       @item.valid?
-      expect(@item.errors.full_messages).to include("Price is too short (minimum is 300 characters)")
+      expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
     end
 
     it '販売価格は半角数字のみ保存可能であること' do
       @item.price = 'あAa'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Price is too short (minimum is 300 characters)")
+      expect(@item.errors.full_messages).to include("Price is not a number")
     end
   end
 end
