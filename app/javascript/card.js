21e573442386d50d95139ae47274ddd9ace0c1da
hsuzawa0429 <hiroyuki.suzawa0429@gmail.com>
2021-08-14 19:48:08 +0900

    card.jsの修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,11 +1,11 @@
 const pay = () => {
-  Payjp.setPublickey("pk_test_0f65827a34b2010c106c9930");
+  Payjp.setPublicKey("pk_test_0f65827a34b2010c106c9930");
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
 
     const formResult = document.getElementById("charge-form");
-    const formData = new FromData(formResult);
+    const formData = new FormData(formResult);
 
     const card = {
       number: formData.get("buyer_destination[number]"),
@@ -18,15 +18,14 @@ const pay = () => {
       if (status == 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
-        debugger;
       }
 
-      document.getElementById("buyer_destination[number]").removeAttribute("name");
-      document.getElementById("buyer_destination[exp_month]").removeAttribute("name");
-      document.getElementById("buyer_destination[exp_year]").removeAttribute("name");
-      document.getElementById("buyer_destination[cvc]").removeAttribute("name");
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
 
       document.getElementById("charge-form").submit();
     });
