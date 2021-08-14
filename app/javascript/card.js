e251ab5c96819d5bffe461ae716f63a9c1d3c8dc
hsuzawa0429 <hiroyuki.suzawa0429@gmail.com>
2021-08-14 18:15:20 +0900

    トークン化の処理実装

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,25 @@
 const pay = () => {
+  Payjp.setPublickey("pk_test_0f65827a34b2010c106c9930");
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FromData(formResult);
+
+    const card = {
+      number: formData.get("buyer_destination[number]"),
+      exp_month: formData.get("buyer_destination[exp_month]"),
+      exp_year: `20${formData.get("buyer_destination[exp_year]")}`,
+      cvc: formData.get("buyer_destination[cvc]"),
+    };
+    
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
   });
 };
 
